175dc03dfe3ac43d0a8e4ffe9f0e4512b3eb7f71
SAHASRANSU KAR <sahasransukar777@gmail.com>
2021-05-20 00:31:09 +0530

    Create salary_count.c

---
+++ salary_count.c
@@ -0,0 +1,11 @@
+// Program to comtute gross salary taking DA=40% of basics HR=30% of basics
+#include <stdio.h>
+
+int main() 
+{
+    int a= 1000,hr,da;
+    hr = 1000 * 0.4;
+    da = 1000 * 0.3;
+    printf("a = %d, b = %d",hr,da);
+    return 0;
+}
